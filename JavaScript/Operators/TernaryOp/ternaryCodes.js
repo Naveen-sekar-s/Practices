cc6185861cc88fc711faaa17a27bb26c44df843c
Naveen-sekar-s <naveensekar369@gmail.com>
2024-02-16 12:54:48 +0530

    Folder-changed

---
+++ JavaScript/Operators/TernaryOp/ternaryCodes.js
@@ -0,0 +1,13 @@
+let player = "scissors";
+let computer = "paper";
+let result =
+  player === computer
+    ? "Tie"
+    : player === "rock" && computer === "paper"
+    ? "computer wins"
+    : player === "paper" && computer === "scissor"
+    ? "computer wins"
+    : player === "scissors" && computer === "rock"
+    ? "computer wins"
+    : "player wins";
+console.log(result);
