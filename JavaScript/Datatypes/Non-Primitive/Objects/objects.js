38c037f9068125c2a12f239862d37f20718ecd6c
Naveen-sekar-s <naveensekar369@gmail.com>
2024-03-21 17:43:29 +0530

    'obj'

--- JavaScript/Datatypes/Non-Primitive/Objects/objects.js
+++ JavaScript/Datatypes/Non-Primitive/Objects/objects.js
@@ -2,6 +2,7 @@
 
 // Object properties are written as name:value pairs, separated by commas.
 
+//eg 1
 const person = {
   firstName: "John",
   lastName: "Doe",
@@ -10,3 +11,15 @@ const person = {
 };
 console.log(person.firstName);
 console.log(typeof person);
+
+//eg 2
+const person2 = {
+  firstName: "John",
+  lastName: "Doe",
+  id: 5566,
+  fullName: function () {
+    return this.firstName + " " + this.lastName;
+  },
+};
+
+console.log(person.id);
