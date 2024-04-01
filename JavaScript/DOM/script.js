e39fbac160fb4aaa75476fe0b757fd123b7ce5cd
Naveen-sekar-s <naveensekar369@gmail.com>
2024-04-01 16:59:03 +0530

    dom

---
+++ JavaScript/DOM/script.js
@@ -0,0 +1,17 @@
+// id
+const id = document.getElementById("id");
+id.style.backgroundColor = "red";
+id.style.color = "white";
+id.style.padding = "20px";
+id.style.margin = "20px";
+
+// query
+const qry = document.querySelector("[query]");
+qry.style.backgroundColor = "green";
+qry.style.color = "white";
+qry.style.padding = "20px";
+qry.style.margin = "20px";
+
+// class
+const cls = document.getElementsByClassName("cls_Selector");
+console.log(cls);
